9bf78037d25473ef577c3786a327a55ffc656f3f
Craig Fletcher <hi@leakypixel.net>
2020-09-15 18:17:07 +0100

    Attempt env var fixing

--- traefik/run.sh
+++ traefik/run.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bashio
 
-ls -la /share
-ls -laR /share
-
 bashio::log.info "Ensuring SSL directory..."
 mkdir -p /ssl/traefik/
 
@@ -13,11 +10,10 @@ bashio::log.info "Static config generated"
 bashio::log.info "Extracting environment variables..."
 ENV_VARS=$(j2 /etc/traefik/env.j2 /data/options.json)
 
-echo ${ENV_VARS}
 if [ -z "$ENV_VARS" ]; then
-  bashio::log.info "No additional environment variables found"
+    bashio::log.info "No additional environment variables found"
 else
-  bashio::log.info "Extracted variables ${ENV_VARS}"
+    bashio::log.info "Extracted variables ${ENV_VARS}"
 fi
 
 bashio::log.info "Starting Nginx for dashboard..."
@@ -25,8 +21,8 @@ nginx -g 'pid /tmp/nginx.pid;'
 
 bashio::log.info "Starting Traefik..."
 if [ -z "$ENV_VARS" ]; then
-  /usr/local/bin/traefik
+    bashio::log.info "Running Traefik without env_vars"
+    /usr/local/bin/traefik
 else
-  export "${ENV_VARS}"
-  exec /usr/local/bin/traefik
+    env $ENV_VARS /usr/local/bin/traefik
 fi
